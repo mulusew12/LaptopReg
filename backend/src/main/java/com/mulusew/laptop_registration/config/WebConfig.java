c779efd50d02cf1019e80e2918e3b30fb3970cd8
mulusew12 <mulegit3@gmail.com>
2026-01-19 12:57:22 -0800

    cc

--- backend/src/main/java/com/mulusew/laptop_registration/config/WebConfig.java
+++ backend/src/main/java/com/mulusew/laptop_registration/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173") // ✅ Specify exact frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
